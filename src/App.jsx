9460648dd147f7af9a4cbf42258052ea4c4a778c
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2024-10-01 15:58:04 +0200

    add investment input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+    <Header />
+
+    <UserInput />
+    </>
+  );
 }
 
-export default App
+export default App;
